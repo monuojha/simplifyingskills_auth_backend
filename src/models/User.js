4cfaf4d19f357968cc276624f5014bd7ccfdb656
monuojha <monuojha8602@gmail.com>
2025-12-27 13:35:04 +0530

    add folder stracture

--- src/models/User.js
+++ src/models/User.js
@@ -1,70 +1,76 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     studentName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  class: {
-    type: String,
+      type: String,
+      required: [true, "Student Name is required"],
+      trim: true,
+    },
+    class: {
+      type: String,
+      
+    },
+    school: {
+      type: String,
+      required: [true, "School is required"],
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      required: [true, "Mobile number is required"],
+      unique: true,
+
+            },
    
-    enum: {
-      values: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'Other'],
-      message: 'Please select a valid class (1-12 or Other)'
-    }
-  },
-  school: {
-    type: String,
- 
-  },
-  address: {
-    type: String,
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
 
+    },
+    gender: {
+      type: String,
+      required: [true, "Gender is required"],
+      enum: {
+        values: ["Male", "Female", "Other"],
+        message: "Gender must be Male, Female, or Other",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters long"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    otp: {
+      type: String,
+    },
+    otpExpiresAt: {
+      type: Date,
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-  mobile: {
-    type: String,
-    required: [true, 'Mobile number is required'],
-    unique: true, // Prevents duplicate mobile numbers
-
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true, // Prevents duplicate emails
-    lowercase: true,
-    trim: true,
-  },
-  gender: {
-    type: String,
-  
-    enum: {
-      values: ['Male', 'Female', 'Other'],
-      message: 'Gender must be Male, Female, or Other'
-    }
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [8, 'Password must be at least 8 characters long']
-  },
-   isVerified: { type: Boolean, default: false }, 
-  otp: { type: String }, 
-  otpExpiresAt: { type: Date },
-  refreshToken: { type: String }, 
-
-
-}
-,
-
-{
-  timestamps: true
-}
-
-
+  {
+    timestamps: true, // Adds createdAt and updatedAt automatically
+  }
 );
 
-const User = mongoose.model('User', userSchema);
+
+
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
-
